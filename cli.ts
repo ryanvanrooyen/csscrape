8db62a978bf699258e9d6a71c8da53a1f220f37c
Ryan Van Rooyen <ryan.vanrooyen@icloud.com>
2015-11-02 20:48:43 -0700

    Added a .npmingore file.

---
+++ cli.ts
@@ -0,0 +1,6 @@
+
+import { WebScraper } from './webScraper';
+
+var scraper = new WebScraper();
+
+console.log('TODO: Implement main CLI program');
--- main.ts
+++
@@ -1,8 +0,0 @@
-
-import { HttpClient } from './httpClient';
-import { WebScraper } from './webScraper';
-
-var httpClient = new HttpClient();
-var scraper = new WebScraper(httpClient);
-
-console.log('TODO: Implement main CLI program');
