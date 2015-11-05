09ec38dade719587468ebcd9c555742b0202d5f2
Ryan Van Rooyen <ryan.vanrooyen@icloud.com>
2015-11-04 21:26:18 -0700

    Started working on a cli for the program.

--- Source/cli.ts
+++ Source/cli.ts
@@ -1,21 +1,36 @@
 
 import * as util from 'util';
-// import * as readline from 'readline';
+import * as program from 'commander';
 import { WebScraper } from './webScraper';
-
-// var rl = readline.createInterface({
-// 	input: process.stdin,
-// 	output: process.stdout
-// });
-
-interface IMovieResult {
-	name: string,
-	seasons: string[]
-}
 
 var scraper = new WebScraper();
 
-scraper.get('http://www.themoviedb.org/search', { query: 'cosmos' })
+function parseSelector(arg) {
+	console.log(arg);
+	return arg;
+}
+
+function runScraper(url) {
+	console.log(url);
+}
+
+program
+	.version('0.1.0')
+	.usage("cssscrape url -s 'selector'")
+	.option('-f, --find <selector>', 'A css selector string')
+	.option('-s, --select <selector>', 'A css selector string or json object', parseSelector)
+	.option('-l, --followlink <selector>', 'A css selector string')
+	.option('-v, --verbose', 'Set logging to verbose')
+	.command('<url> [options]')
+	.action(runScraper)
+	.parse(process.argv);
+
+if (!program.args || !program.args.length) {
+    program.help();
+}
+
+
+/*scraper.get('http://www.themoviedb.org/search', { query: 'cosmos' })
 	.find('.results .item:nth-child(-n+3)')
 	.select({
 		name: '.title',
@@ -26,15 +41,10 @@ scraper.get('http://www.themoviedb.org/search', { query: 'cosmos' })
 		poster: '#leftCol .poster img[src]',
 		'seasons[]': '.season_list li .info h4'
 	})
-	.done<IMovieResult>()
-		.then(results => {
-			console.log(util.inspect(results, false, null));
-		})
-		.then(null, error => {
-			console.log(error);
-		});
-
-// rl.question("Press enter to quit...", resp => {
-// 	console.log(resp);
-// 	rl.close();
-// });
+	.done<any>()
+	.then(results => {
+		console.log(util.inspect(results, false, null));
+	})
+	.then(null, error => {
+		console.log(error);
+	});*/
