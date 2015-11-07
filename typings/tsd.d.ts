b9fa3f61eebc34f7c8d8b310dd1bbafdbfd7b89c
Ryan Van Rooyen <ryan.vanrooyen@icloud.com>
2015-11-07 15:44:26 -0700

    Adding typings to GitHub to prevent tsd rate limit error on build.

---
+++ typings/tsd.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="chai/chai.d.ts" />
+/// <reference path="cheerio/cheerio.d.ts" />
+/// <reference path="commander/commander.d.ts" />
+/// <reference path="mocha/mocha.d.ts" />
+/// <reference path="node/node.d.ts" />
